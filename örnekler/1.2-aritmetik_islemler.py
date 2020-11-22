1f9095f49f84031c698c180475ffeb517a2ab847
Ayhan Çelik <ayhancelik542@gmail.com>
2020-11-22 21:14:51 +0300

    Aritmetik işlemler eklendi.

---
+++ örnekler/1.2-aritmetik_islemler.py
@@ -0,0 +1,40 @@
+# Bu program toplama, çıkarma, çarpma, bölme, tam bölme, mod alma ve üs alma işlemlerini yapmaktadır.
+
+sayi1 = 10
+sayi2 = 3
+
+# Toplama
+sonuc1 = sayi1 + sayi2
+
+# Çıkarma
+sonuc2 = sayi1 - sayi2
+
+# Çarpma
+sonuc3 = sayi1 * sayi2
+
+# Bölme
+sonuc4 = sayi1 / sayi2
+
+# Tam Bölme
+sonuc5 = sayi1 // sayi2
+
+# Mod Alma
+sonuc6 = sayi1 % sayi2
+
+# Üs Alma
+sonuc7 = sayi1 ** sayi2
+
+# Sonuçlar
+print ( 'Toplama: {0} + {1} = {2}'.format ( sayi1, sayi2, sonuc1 ) )
+
+print ( 'Çıkarma: {0} - {1} = {2}'.format ( sayi1, sayi2, sonuc2 ) )
+
+print ( 'Çarpma: {0} * {1} = {2}'.format ( sayi1, sayi2, sonuc3 ) )
+
+print ( 'Bölme: {0} / {1} = {2}'.format ( sayi1, sayi2, sonuc4 ) )
+
+print ( 'Tam Bölme: {0} // {1} = {2}'.format ( sayi1, sayi2, sonuc5 ) )
+
+print ( 'Mod alma: {0} % {1} = {2}'.format ( sayi1, sayi2, sonuc6 ) )
+
+print ( 'Üs alma: {0} ** {1} = {2}'.format ( sayi1, sayi2, sonuc7 ) )
